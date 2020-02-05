a99f39764661a782c0486dff3b8699abdf087b01
Widvin Quiñónez <29551485+widjos@users.noreply.github.com>
2020-02-05 05:35:07 -0600

    change methods

--- src/Graphic_Interface/MainWindowController.java
+++ src/Graphic_Interface/MainWindowController.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
