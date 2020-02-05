cf0af4d522399801933db7648cef527962990473
Widvin Quiñónez <29551485+widjos@users.noreply.github.com>
2020-02-05 04:00:46 -0600

    added the conection with the fxml and the controller

--- src/Graphic_Interface/MainWindowController.java
+++ src/Graphic_Interface/MainWindowController.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Graphic_Interface;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 
 /**
  * FXML Controller class
@@ -19,6 +19,11 @@ public class MainWindowController implements Initializable {
     /**
      * Initializes the controller class.
      */
+     @FXML  Button btnCompile;
+     @FXML  TextArea txtCodeInput, txtConsole;
+     @FXML MenuItem itemCompilar, itemOpen;
+     
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
